13f8822cb734f655aa7f36de11fa35a63de722c4
joshbesse <joshua.besse3@gmail.com>
2024-09-26 16:32:37 -0400

    added simulation data for migration

--- algo_trading/algo_trading/settings.py
+++ algo_trading/algo_trading/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'algo_trading.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
