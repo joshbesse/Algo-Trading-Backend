a3f7eb13b65ed253b77d5fc6f8de4d50408a6aa7
joshbesse <joshua.besse3@gmail.com>
2024-09-26 15:44:34 -0400

    updated settings

--- algo_trading/algo_trading/settings.py
+++ algo_trading/algo_trading/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os 
 import dj_database_url
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,18 +79,9 @@ WSGI_APPLICATION = 'algo_trading.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ and 'sqlite' not in os.environ['DATABASE_URL']:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    # Use SQLite as the default database
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
