61a4a3fd83a0d5d3ef2e047f6b342914d1e55df6
joshbesse <joshua.besse3@gmail.com>
2024-09-26 17:51:51 -0400

    updated MIDDLEWARE and CORS_ALLOWED_ORIGINS

--- algo_trading/algo_trading/settings.py
+++ algo_trading/algo_trading/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,4 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://algo-trading.netlify.app'
 ]
