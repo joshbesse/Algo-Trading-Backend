0bfdd7b1e59e4f8e5a1987e6dd9d283bac565491
joshbesse <joshua.besse3@gmail.com>
2024-09-26 23:33:36 -0400

    changed directory structure and updated configurations

--- algo_trading/wsgi.py
+++ algo_trading/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'algo_trading.algo_trading.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'algo_trading.settings')
 
 application = get_wsgi_application()
